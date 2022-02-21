0fb1723850426599119e29c339c9898e197cec40
Yulian Melnychuk <yulian.melnychuk@ptp.diagnostic>
2022-02-21 20:22:09 +0100

    update

--- src/AppFramer.js
+++ src/AppFramer.js
@@ -6,20 +6,31 @@ import {motion ,AnimatePresence} from "framer-motion"
 import images from "./images"
 
 function AppFrmaer() {
+
+    const [width ,setWidth]= React.useState(0)
+    const carousel =React.useRef()
+
+    React.useEffect(()=>{
+        // console.log(carousel.current)
+        setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth)
+    },[])
   return (
     <div className="App" >
-            <motion.div className="carousel">
-                <motion.div className="inner-carousel">
-                    {images.map((image)=>{
-                        return(
-                            <motion.div className="item" key={image}>
-                                <img src={image} alt/>
-                            </motion.div>
-                        )
-                    })}
-                </motion.div>
+        <div className='container'>
+            <motion.div className="carousel" ref={carousel } whileTap={{cursor:"grabbing"}}>
+                    <motion.div drag="x" dragConstraints={{right:1000 ,left:-width}} className="inner-carousel">
+                        {images.map((image)=>{
+                            return(
+                                <motion.div  className="item" key={image}>
+                                    <img src={image} alt="cos"/>
+                                </motion.div>
+                            )
+                        })}
+                    </motion.div>
 
-            </motion.div>
+                </motion.div>
+        </div>
+           
     </div>
   )
 }
