511824b6f0934c10f816544de83fdc02edd2f06c
yulian911 <74702716+yulian911@users.noreply.github.com>
2022-02-21 15:01:22 +0100

    new app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import Movie from './Movie'
+import './App.css'
+import Filter from './Filter'
+import {motion ,AnimatePresence} from "framer-motion"
 
 function App() {
+
+  const[popular,setPopular]=React.useState([])
+  const[filter,setFilter]=React.useState([])
+  const[activeGenre,setActiveGenre]=React.useState(0)
+
+  const fetchPopular = async()=>{
+      const data = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=d5c35e51c81488b19da7c1f572507a3d&language=en-US&page=1');
+      
+      const movies = await data.json()
+      setPopular(movies.results)
+      setFilter(movies.results)
+      console.log(popular)
+
+  }
+
+  useEffect(()=>{
+  fetchPopular()
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <Filter popular={popular} setFiltered={setFilter} activeGenre={activeGenre} setActiveGenre={setActiveGenre}/>
+      <motion.div 
+      
+       layout className="popular-movies">
+         <AnimatePresence>
+            {filter.map(movie=>{
+              return(
+                  <Movie key={movie.id} movie={movie}/>
+              )
+            })}
+         </AnimatePresence>
+     
+      </motion.div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+// https://api.themoviedb.org/3/movie/popular?api_key=<<d5c35e51c81488b19da7c1f572507a3d>>&language=en-US&page=1
---
+++ src/Movie.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {motion} from "framer-motion"
+
+function Movie({movie}) {
+  return (
+    <motion.div 
+    animate ={{opacity:1}} 
+    initial={{opacity:0}}
+    exit={{opacity:0}}
+    layout>
+        <h2>{movie.title}</h2>
+        <img src={'https://image.tmdb.org/t/p/w500'+ movie.backdrop_path} alt={movie.title} />
+    </motion.div>
+  )
+}
+
+export default Movie
